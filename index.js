f763a696ac15533e0ebc207cc5afb903a9a004be
gotgandata <gotgandata@naver.com>
2024-10-30 09:54:34 +0900

    update port2

--- index.js
+++ index.js
@@ -55,5 +55,7 @@ app.post("/matchUser", async (req, res) => {
 })
 
 // 서버 시작
-const PORT = 10000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`)
+})
