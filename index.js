13ab111ac33cc807e0977b036190fb3d81908a37
gotgandata <gotgandata@naver.com>
2024-10-30 10:36:32 +0900

    update post

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 })
 
 // 사용자 추가 API
-app.get("/addUser", async (req, res) => {
+app.post("/addUser", async (req, res) => {
   const { name } = req.body
   const user = new User({ name })
   await user.save()
